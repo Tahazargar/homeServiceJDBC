e2f9b55ae6f4fd3343c335833bc4233c62abcd33
TahaZargar <mr.tahazargar@gmail.com>
2025-07-30 13:08:29 +0330

    Project strucutre changed.

--- src/main/java/com/homeservice/util/DatabaseUtil.java
+++ src/main/java/com/homeservice/util/DatabaseUtil.java
@@ -1,4 +1,23 @@
 package main.java.com.homeservice.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseUtil {
+    private static final String URL = "jdbc:postgresql://localhost:5432/home_service";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
+    static {
+        try{
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
